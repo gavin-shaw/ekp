7e0ed89a5295b791a6e469863af8c5af0595b9b7
gavin-shaw <gavin@gnarus.io>
2022-01-13 01:48:32 +0200

    fix: move back to npm to fix build

--- apps/critterz/src/prices/prices.document.ts
+++ apps/critterz/src/prices/prices.document.ts
@@ -3,4 +3,5 @@ import { DocumentDto } from '@app/sdk';
 export interface PricesDocument extends DocumentDto {
   readonly blockPrice: number;
   readonly scritterzPrice: number;
+  readonly fiatSymbol: number;
 }
--- apps/critterz/src/ui/critterz.menus.ts
+++ apps/critterz/src/ui/critterz.menus.ts
@@ -1,8 +1,21 @@
+import { formatCurrency } from '@app/sdk/ui';
+import { PRICES_DOCUMENT } from '../util/collectionNames';
+
 export default function menus() {
   return [
     {
       header: 'Critterz',
     },
+    // {
+    //   component: PriceLink({
+    //     price: formatCurrency(
+    //       `$.${PRICES_DOCUMENT}[0].blockPrice`,
+    //       `$.${PRICES_DOCUMENT}[0].fiatSymbol`,
+    //     ),
+    //     href: `https://www.dextools.io/app/ether/pair-explorer/0xe93527d1f8c586353b13826c501fa5a69bce2b0e`,
+    //     label: '$BLOCK',
+    //   }),
+    // },
     {
       title: 'Rental Checker',
       navLink: 'critterz/rental-checker',
