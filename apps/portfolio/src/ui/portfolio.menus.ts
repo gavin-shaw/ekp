9319a778e48a9e21036cdfe7c547990e82d38b26
gavin-shaw <gavin@gnarus.io>
2022-01-13 21:45:56 +0200

    feat: homepage

--- apps/portfolio/src/ui/portfolio.menus.ts
+++ apps/portfolio/src/ui/portfolio.menus.ts
@@ -1,14 +1,17 @@
 export default function menus() {
   return [
     {
-      header: 'Balances',
+      id: 'portfolio',
+      header: 'Portfolio',
     },
     {
+      id: 'portfolio-token-balances',
       title: 'Tokens',
       navLink: 'portfolio/tokens/balances',
       icon: 'cil-money',
     },
     {
+      id: 'portfolio-nft-balances',
       title: 'NFTs',
       navLink: 'portfolio/nfts/balances',
       icon: 'cil-color-palette',
