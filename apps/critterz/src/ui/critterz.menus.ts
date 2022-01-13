e9adf49ef96ba8fb1fd19428d5eb1078e32e38bd
gavin-shaw <gavin@gnarus.io>
2022-01-13 21:48:05 +0200

    feat: critterz price

--- apps/critterz/src/ui/critterz.menus.ts
+++ apps/critterz/src/ui/critterz.menus.ts
@@ -1,8 +1,22 @@
+import { formatCurrency, PriceLink } from '@app/sdk/ui';
+import { PRICES_DOCUMENT } from '../util';
+
 export default function menus() {
   return [
     {
       id: 'critterz',
       header: 'Critterz',
+    },
+    {
+      id: 'critterz-prices',
+      component: PriceLink({
+        price: formatCurrency(
+          `$.${PRICES_DOCUMENT}[0].blockPrice`,
+          `$.${PRICES_DOCUMENT}[0].fiatSymbol`,
+        ),
+        href: `https://www.dextools.io/app/ether/pair-explorer/0xe93527d1f8c586353b13826c501fa5a69bce2b0e`,
+        label: 'BLOCK PRICE',
+      }),
     },
     {
       id: 'critterz-rental-checker',
